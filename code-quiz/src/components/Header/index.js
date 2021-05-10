10d43f6078d5bd4749ba24cbd547377a64e3779e
Lionel Townsend <townsendlionel@gmail.com>
2021-05-10 13:19:29 -0500

    altered header

--- code-quiz/src/components/Header/index.js
+++ code-quiz/src/components/Header/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        Header Component
+    <header class="container">
+      <div class="row ">
+       <a class="col-2" href="">View HighScore</a>
+       <div class="col-8"/>
+       <div class="col-2">Time: 0</div>
       </div>
     </header>
   );
